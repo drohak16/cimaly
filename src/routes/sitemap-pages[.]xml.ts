5b58677f2c2e67fd734245e2d9963e14d3d487e7
drohak16 <drohak16@gmail.com>
2026-08-15 19:11:11 +0300

    Add sitemap route for XML file generation

---
+++ src/routes/sitemap-pages[.]xml.ts
@@ -0,0 +1,36 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+const BASE_URL = "https://cimaly.cc";
+
+export const Route = createFileRoute("/sitemap-pages.xml")({
+  server: {
+    handlers: {
+      GET: async () => {
+        const urls = [
+          `${BASE_URL}/`,
+          `${BASE_URL}/movies`,
+          `${BASE_URL}/tv`,
+        ];
+
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${url}</loc>
+  </url>`,
+  )
+  .join("\n")}
+</urlset>`;
+
+        return new Response(xml, {
+          status: 200,
+          headers: {
+            "Content-Type": "application/xml; charset=UTF-8",
+            "Cache-Control": "no-cache",
+          },
+        });
+      },
+    },
+  },
+});

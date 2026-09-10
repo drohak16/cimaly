0cba76ee82846f13c46321698a8cde77db2982ad
drohak16 <drohak16@gmail.com>
2026-09-10 17:37:36 +0300

    Remove TV 103815 from Cimaly

--- src/routes/tv.$id.tsx
+++ src/routes/tv.$id.tsx
@@ -2,9 +2,11 @@ import { createFileRoute, notFound } from "@tanstack/react-router";
 import { DetailPage } from "@/components/DetailPage";
 import { IMG, tmdb } from "@/lib/tmdb";
 
+const REMOVED_TV_IDS = new Set(["103815"]);
+
 export const Route = createFileRoute("/tv/$id")({
   loader: async ({ params }) => {
-    if (!/^\d+$/.test(params.id)) {
+    if (!/^\d+$/.test(params.id) || REMOVED_TV_IDS.has(params.id)) {
       throw notFound();
     }
 
